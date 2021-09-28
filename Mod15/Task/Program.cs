ffa8a77dd536474de4ee08259efd5e5304e84060
skripkalisa <skripkalisa.msk@ya.ru>
2021-09-28 23:02:40 +0300

    Mod15 task is daone

---
+++ Mod15/Task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mod15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var classes = new []
+            {
+                new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+                new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+                new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+            var allStudents = GetAllStudents(classes);
+         
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+        static string [] GetAllStudents( Classroom [] classes )
+        {
+            return classes.SelectMany(c => c.Students).ToArray();
+        }
+
+        private class Classroom
+        {
+            public readonly List<string> Students = new();
+        }
+    }
+}
